c89b0be227fd541ab5b4e008742598483e2ae3e9
Michele Locati <michele@locati.it>
2014-08-22 08:04:26 +0200

    Allow executing tests without composer
    ...quite dirty but handy while developing ;)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
 error_reporting(E_ALL);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+if(is_file(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require_once dirname(__DIR__) . '/vendor/autoload.php';
+}
+elseif(is_file(dirname(dirname(__DIR__)) . '/common/code/Data.php')) {
+    require_once dirname(dirname(__DIR__)) . '/common/code/Data.php';
+    require_once dirname(__DIR__) . '/code/Calendar.php';
+}
 
 PHPUnit_Framework_Error_Notice::$enabled = true;
 
