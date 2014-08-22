d8b104c49dae8ff2b62e8f0cd09e8ef30bb33902
Michele Locati <michele@locati.it>
2014-08-22 13:57:06 +0200

    Bugfix

--- tests/Calendar/CalendarTest.php
+++ tests/Calendar/CalendarTest.php
@@ -1099,16 +1099,16 @@ class CalendarTest extends PHPUnit_Framework_TestCase
         $this->assertSame('Fiji', Calendar::format($dt, 'VVV'));
         $this->assertSame('GMT+13:00', Calendar::format($dt, 'VVVV'));
         // decodeTimezoneWithTime
-        $this->assertSame('++13', Calendar::format($dt, 'x'));
+        $this->assertSame('+13', Calendar::format($dt, 'x'));
         $this->assertSame('+1300', Calendar::format($dt, 'xx'));
         $this->assertSame('+13:00', Calendar::format($dt, 'xxx'));
-        $this->assertSame('++1300', Calendar::format($dt, 'xxxx'));
-        $this->assertSame('++13:00', Calendar::format($dt, 'xxxxx'));
+        $this->assertSame('+1300', Calendar::format($dt, 'xxxx'));
+        $this->assertSame('+13:00', Calendar::format($dt, 'xxxxx'));
         // decodeTimezoneWithTimeZ
-        $this->assertSame('++13', Calendar::format($dt, 'X'));
+        $this->assertSame('+13', Calendar::format($dt, 'X'));
         $this->assertSame('+1300', Calendar::format($dt, 'XX'));
         $this->assertSame('+13:00', Calendar::format($dt, 'XXX'));
-        $this->assertSame('++1300', Calendar::format($dt, 'XXXX'));
-        $this->assertSame('++13:00', Calendar::format($dt, 'XXXXX'));
+        $this->assertSame('+1300', Calendar::format($dt, 'XXXX'));
+        $this->assertSame('+13:00', Calendar::format($dt, 'XXXXX'));
     }
 }
