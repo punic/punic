fae6ed5115f0996f3daebb33e605a3b5fa33e7c4
Michele Locati <michele@locati.it>
2014-08-28 18:25:24 +0200

    Add Number::unformat

--- code/Number.php
+++ code/Number.php
@@ -37,4 +37,30 @@ class Number
 
         return $result;
     }
+
+    /**
+     * Convert a localized representation of a number to a number (for instance, converts the string '1,234' to 1234 in case of English and to 1.234 in case of Italian)
+     * @param string $value The string value to convert
+     * @param string $locale = '' The locale to use. If empty we'll use the default locale set in \Punic\Data
+     * @return int|float|null Returns null if $value is not valid, the numeric value otherwise
+     */
+    public static function unformat($value, $locale = '')
+    {
+        $result = null;
+        if (is_int($value) || is_float($value)) {
+            $result = $value;
+        } elseif (is_string($value) && static::isNumeric($value, $locale)) {
+            $data = \Punic\Data::get('numbers', $locale);
+            $decimal = $data['symbols']['decimal'];
+            $minus = $data['symbols']['minusSign'];
+            $simplified = strtoupper(preg_replace('/[^0-9eE'. preg_quote($decimal) . preg_quote($minus) . ']/', '', $value));
+            if ((strpos($simplified, $decimal) === false) && (strpos($simplified, 'E') === false)) {
+                $result = intval($simplified);
+            } else {
+                $result = floatval(str_replace($decimal, '.', $simplified));
+            }
+        }
+
+        return $result;
+    }
 }
