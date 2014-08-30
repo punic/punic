aad2863b3102cf356541736e8bbc6324db1a07c5
Remo Laubacher <remo.laubacher@gmail.com>
2014-08-30 09:00:15 +0200

    add some unit tests for unit and territory

--- tests/Territory/TerritoryTest.php
+++ tests/Territory/TerritoryTest.php
@@ -26,4 +26,23 @@ class TerritoryTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testCountries()
+    {
+        $countries = Territory::getCountries();
+        $this->assertArrayHasKey('CA', $countries);
+        
+        // this list isn't static, we assume that something between 240 and 280 countries is okay
+        $this->assertLessThan(280, count($countries));
+        $this->assertGreaterThan(240, count($countries));
+    }
+    
+    public function testContinents()
+    {
+        $continents = Territory::getContinents();
+        $this->assertContains('Africa', $continents);
+        
+        // this list isn't static, we assume that something between 3 and 7 continents is okay
+        $this->assertLessThan(count($continents), 3);
+        $this->assertGreaterThan(count($continents), 7);
+    }
 }
--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -57,7 +57,7 @@ class UnitTest extends PHPUnit_Framework_TestCase
             Unit::format($parameters[0], $parameters[1], $parameters[2], $parameters[3])
         );
     }
-    
+
     public function testValueNotInListExceptionGetValue()
     {
         try
@@ -70,11 +70,17 @@ class UnitTest extends PHPUnit_Framework_TestCase
             $this->assertSame(array('long', 'short', 'narrow'), $ex->getAllowedValues());            
         }
     }
-    
+
     public function testValueNotInListException()
     {
         $this->setExpectedException('\\Punic\\Exception\\ValueNotInList');
         Unit::format(2, 'milisecond', 'does-not-exist');
+    }
+
+    public function testInvalidUnit()
+    {
+        $this->setExpectedException('\\Punic\\Exception\\ValueNotInList');
+        Unit::format(2, 'invalid-unit');
     }
 
 }
