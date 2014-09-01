bc61ae0e8be72f18b3685f4231cce59fb4f0183a
Remo Laubacher <remo.laubacher@gmail.com>
2014-09-01 08:39:08 +0200

    full coverage for Misc class

--- tests/Misc/ListTest.php
+++ tests/Misc/ListTest.php
@@ -137,4 +137,29 @@ class ListTest extends PHPUnit_Framework_TestCase
 		$this->setExpectedException('\\Punic\\Exception\\ValueNotInList');
 		Misc::joinUnits(array('One', 'Two'), 'invalid-width', 'en');	
 	}
+    
+    public function providerFixCase()
+    {
+        return array(
+            array('Test', 'test', 'titlecase-words'),
+            array('Test Test', 'test test', 'titlecase-words'),
+            array('TEST TEST', 'TEST TEST', 'titlecase-words'),
+            array('A', 'a', 'titlecase-words'),
+            array('Test', 'test', 'titlecase-firstword'),
+            array('Test test', 'test test', 'titlecase-firstword'),
+            array('TEST TEST', 'TEST TEST', 'titlecase-firstword'),
+            array('A', 'a', 'titlecase-firstword'),
+            array('a', 'a', 'lowercase-words'),
+            array('a', 'A', 'lowercase-words'),
+            array('test test', 'Test test', 'lowercase-words'),
+        );
+    }
+    
+    /**
+     * @dataProvider providerFixCase
+     */
+    public function testFixCase($result, $string, $case)
+    {
+        $this->assertSame($result, \Punic\Misc::fixCase($string, $case));
+    }
 }
