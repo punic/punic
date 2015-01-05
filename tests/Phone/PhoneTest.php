ee92af3c6676fe6b11664872a2d3a048f5d3ca3a
Michele Locati <michele@locati.it>
2015-01-05 22:34:52 +0100

    Add Punic\Phone

---
+++ tests/Phone/PhoneTest.php
@@ -0,0 +1,45 @@
+<?php
+use \Punic\Phone;
+
+class PhoneTest extends PHPUnit_Framework_TestCase
+{
+    public function somePrefixes()
+    {
+        return array(
+            array('US', '1'),
+            array('CA', '1'),
+            array('IT', '39'),
+            array('DE', '49'),
+            array('001', '388'),
+        );
+    }
+
+    /**
+     * @dataProvider somePrefixes
+     */
+    public function testGetPrefixesForTerritory($territoryCode, $prefix)
+    {
+        $this->assertContains(
+            $prefix,
+            Phone::getPrefixesForTerritory($territoryCode)
+        );
+    }
+
+    /**
+     * @dataProvider somePrefixes
+     */
+    public function testGetTerritoriesForPrefix($territoryCode, $prefix)
+    {
+        $this->assertContains(
+            $territoryCode,
+            Phone::getTerritoriesForPrefix($prefix)
+        );
+    }
+
+    public function testGetMaxPrefixLength()
+    {
+        $maxLength = Phone::getMaxPrefixLength();
+        $this->assertGreaterThanOrEqual(1, $maxLength);
+        $this->assertLessThanOrEqual(6, $maxLength);
+    }
+}
