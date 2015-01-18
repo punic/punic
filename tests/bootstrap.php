60c944db24d62b7fcc7eba691a0d8d7be12d7df8
Michele Locati <michele@locati.it>
2015-01-18 12:13:02 +0100

    Remove composer requirements for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,13 @@
 <?php
 error_reporting(E_ALL);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+$timezone_identifier = @date_default_timezone_get();
+if(empty($timezone_identifier)) {
+    $timezone_identifier = 'UTC';
+}
+date_default_timezone_set($timezone_identifier);
+unset($timezone_identifier);
+
+require_once dirname(__DIR__) . '/punic.php';
 
 PHPUnit_Framework_Error_Notice::$enabled = true;
