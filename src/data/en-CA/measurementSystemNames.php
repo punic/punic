fe27df252c0bbce0792e535f4e69818d8771051b
Michele Locati <michele@locati.it>
2021-11-16 10:06:16 +0100

    Update CLDR to 40

--- src/data/en-CA/measurementSystemNames.php
+++ src/data/en-CA/measurementSystemNames.php
@@ -1,3 +1,3 @@
 <?php
 // This file is auto-generated. Do not edit!
-return array('metric'=>'Metric','UK'=>'UK','US'=>'US');
+return array('metric'=>'Metric','UK'=>'U.K.','US'=>'U.S.');
--- src/data/ru/measurementSystemNames.php
+++ src/data/ru/measurementSystemNames.php
@@ -1,3 +1,3 @@
 <?php
 // This file is auto-generated. Do not edit!
-return array('metric'=>'Метрическая','UK'=>'Английская','US'=>'Англо-американская');
+return array('metric'=>'Метрическая','UK'=>'Английская','US'=>'Американская');
